a6cc7133b456e178adee7fd2fdd1431e157dc19a
Matheus R <matheusrf96@gmail.com>
2018-06-14 14:19:35 -0300

    Commit inicial

---
+++ paginas/inicio.php
@@ -0,0 +1,28 @@
+<div class="refeicao">
+	Café da Manhã:
+</div>
+<hr />
+
+<div class="refeicao">
+	Lanche da Manhã:
+</div>
+<hr />
+
+<div class="refeicao">
+	Almoço: 
+</div>
+<hr />
+
+<div class="refeicao">
+	Café da Tarde: 
+</div>
+<hr />
+
+<div class="refeicao">
+	Lanche da Tarde: 
+</div>
+<hr />
+
+<div class="refeicao">
+	Jantar:
+</div>
